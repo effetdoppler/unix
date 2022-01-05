22925966d03967087c1dbc589b0dfbaa17b99f55
Charles Zhang <charles.zhang@epita.fr>
2022-01-05 14:59:22 +0100

    check file type

---
+++ io_skeleton/file_type.c
@@ -0,0 +1,34 @@
+#include <err.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+void file_type(char *filename)
+{
+    /*
+     * 1. Take a good look at the stat(2) man-page to see what you need
+     * to declare first.
+     *
+     * 2. Once you called stat and it works, you can check whether it
+     * is a regular file or a directory.
+     *
+     */
+    struct stat path_stat;
+    stat(filename, &path_stat);
+    if (S_ISREG(path_stat.st_mode))
+            write(1, "The given file is a regular file!", 34);
+    else
+        write(1, "The given file is a directory!", 31);
+    write(1, "\n", 1);
+
+    
+}
+
+int main(int argc, char **argv)
+{
+    // TODO: Check that the number of arguments is equal to 2, exit otherwise.
+    file_type("/home/charles.zhang/afs");
+    file_type("/home/charles.zhang/afs/unix/unix/io_skeleton/file_type.c");    
+    return 0;
+}
